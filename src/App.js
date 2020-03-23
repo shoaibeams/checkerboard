8016d3f080ac29c419531c9734db3a1bcf90bff7
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2020-03-24 03:43:29 +0500

    adding custom pieces

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import ChessBoard from 'chessboardjsx'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [width, setWidth] = useState(500)
+  const [error, setError] = useState('')
+
+  const inputHandler = event => {
+    if (event.target.value === '') {
+      setError('')
+    }
+
+    const num = event.target.value
+    if (event.target.value !== '') {
+      if (num < 5 || num > 20) {
+        setError("Number can't be less than 5 or greater than 20")
+      } else {
+        setWidth(event.target.value * 50)
+        setError('')
+      }
+    }
+
+    debugger
+  }
+
+  useEffect(() => {}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      CheckerBoard
+      <div>
+        <input
+          className="input"
+          type="number"
+          placeholder="Change the size of the grid?"
+          onChange={event => inputHandler(event)}
+        />
+        <div style={{ color: 'red' }}>{width ? error : null}</div>
+        <ChessBoard
+          id="standard"
+          orientation="black"
+          position="start"
+          darkSquareStyle={{ backgroundColor: 'black' }}
+          lightSquareStyle={{ backgroundColor: 'white' }}
+          width={width}
+          pieces={{
+            wK: ({ squareWidth, isDragging }) => (
+              <img
+                style={{
+                  width: isDragging ? squareWidth * 1.75 : squareWidth,
+                  height: isDragging ? squareWidth * 1.75 : squareWidth
+                }}
+                src={'red-sphere.png'}
+                alt={'elvis'}
+              />
+            ),
+            rk: ({ squareWidth, isDragging }) => (
+              <img
+                style={{
+                  width: isDragging ? squareWidth * 1.75 : squareWidth,
+                  height: isDragging ? squareWidth * 1.75 : squareWidth
+                }}
+                src={'circle.png'}
+                alt={'elvis'}
+              />
+            )
+          }}
+        />
         >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
